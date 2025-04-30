40ce5d908ef0ea1d4c115e42f5540fe18d5f5f8d
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2025-04-30 03:54:26 +0000

    assignment 03

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+"use strict";
+
+// function to calculate and display surface area and volume of an octagonal prism
+function calculateOctagonalPrism() {
+  // convert user inputs into variables
+  let a = parseFloat(document.getElementById("base").value);
+  let h = parseFloat(document.getElementById("height").value);
+
+  // square root of 2
+  let sqrt2 = Math.sqrt(2);
+
+  // surface area
+  let surfaceArea = 8 * a * h + 4 * (1 + sqrt2) * a * a;
+
+  // volume
+  let volume = 2 * (1 + sqrt2) * a * a * h;
+
+  // show results with if statement
+  let resultText = "Surface Area: " + surfaceArea.toFixed(2) + " cm²\nVolume: " + volume.toFixed(2) + " cm³";
+
+  if (surfaceArea > 500 || volume > 500) {
+    resultText += "\nThat's a big prism!";
+  }
+
+  document.getElementById("result").innerText = resultText;
+}
